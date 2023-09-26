c4bb1e572289baca0e7aea1fa59bd447c85476b5
Miracle6S <139767388+Miracle6S@users.noreply.github.com>
2023-09-26 21:21:49 +0300

    Add files via upload

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,19 +1,20 @@
-2-strlen.c
 #include "main.h"
+
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
  */
+
 int _strlen(char *s)
 {
-	int longi = 0;
+	int l = 0;
 
 	while (*s != '\0')
 	{
-		longi++;
 		s++;
+		l++;
 	}
-
-	return (longi);
+	return (l);
 }
+
